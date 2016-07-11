36ff06af6f114f85ff449434b307af470a2e42d0
samiracoliva <samiracoliva@users.noreply.github.com>
2016-07-10 18:29:14 -0700

    Update lab3.sh

--- lab3.sh
+++ lab3.sh
@@ -1,7 +1,8 @@
 
 #!/bin/bash
 # shell script for testing compiler
-lex lab3.l
+# PARSER.l is lex / flex file with expression grammar
+lex PARSER.l
 gcc -o myCompiler lex.yy.c
 echo -n " ( 5 * 7 \ 3 + 1 ) " | ./myCompiler  
 nasm -f elf ASSEMBLY_FILE.asm 
